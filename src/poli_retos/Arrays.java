ae23edad3f6bc2356af0fcba017131ea403ceb99
Meli-u <melissa.guerra@epn.edu.ec>
2024-11-04 18:58:58 -0500

    Update Arrays.java

--- src/poli_retos/Arrays.java
+++ src/poli_retos/Arrays.java
@@ -1,6 +1,33 @@
 package poli_retos;
 
 public class Arrays {
+
+    public static void g2_arrays03(String nombre) {
+        System.out.println("\nPlano de coordenadas para la función f(x) = 2x:");
+        int ejeY = nombre.length(); 
+        for (int y = 0; y <= ejeY; y++) {
+            if (y < ejeY) {
+                System.out.printf("%3d |", ejeY - y);
+            } else {
+                System.out.printf("%3d |", 0);
+                System.out.print(" _");
+            }
+            for (int x = 0; x < nombre.length(); x++) {
+                if (y == x) { 
+                    System.out.print(" " + nombre.charAt(x) + " ");
+                } else {
+                    System.out.print("   "); 
+                }
+            }
+            System.out.println(); 
+        }
+        System.out.print("      ");
+        for (int x = 0; x < nombre.length(); x++) {
+            System.out.printf("%3d", x); 
+        }
+        System.out.println();
+    }
+    
     public void Array04(){
         char nombre[] = {'G','A','R','Y'};
         char apellido[] = {'D','E','F','A','S'};
