964d0394747d232947139abcf2ccda0502bb5551
Cristhian Carrillo <cristhiancarrillolocopaul@gmail.com>
2024-11-04 13:56:40 -0500

    Ejercicio loading 10

--- src/poliretos/loading.java
+++ src/poliretos/loading.java
@@ -1,23 +1,25 @@
 package poliretos;
 
-public class loading{
+public class loading {
     public String frase;
-    public float carga_actuar=0.00f;
-    public loading(String frase){
-        this.frase=frase;
+    public float carga_actuar = 0.00f;
+
+    public loading(String frase) {
+        this.frase = frase;
         barra_de_nombre();
     }
-    private void barra_de_nombre(){
-        char[] frase=this.frase.toCharArray();
-        for (int carga = 0; carga <this.frase.length() ; carga++) {
+
+    private void barra_de_nombre() {
+        char[] frase = this.frase.toCharArray();
+        for (int carga = 0; carga < this.frase.length(); carga++) {
             for (int espacio = 0; espacio < carga; espacio++) {
                 System.out.print(" ");
             }
             System.out.print(frase[carga]);
-            for (int espacio = 0; espacio < this.frase.length()-carga; espacio++) {
+            for (int espacio = 0; espacio < this.frase.length() - carga; espacio++) {
                 System.out.print(" ");
             }
-            System.out.print(((carga+1)*100)/this.frase.length()+"%");
+            System.out.print(((carga + 1) * 100) / this.frase.length() + "%");
             try {
                 Thread.sleep(1000);
                 System.out.print("\r");
@@ -25,5 +27,8 @@ public class loading{
                 System.err.println("Interrupción durante la carga: " + e.getMessage());
             }
         }
-        }
     }
+    private void carga_documento(){
+        int numero_random;
+    }
+}
