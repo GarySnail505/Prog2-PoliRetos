29b4a4ab3fc4d8267fd5b8b2af1d9efbb9159b0e
Cristhian Carrillo <cristhiancarrillolocopaul@gmail.com>
2024-11-04 12:32:51 -0500

    Ejercicio Array2

--- src/App.java
+++ src/App.java
@@ -1,17 +1,19 @@
+
+import java.util.*;
 import poliretos.*;
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         int longitud=5;
         int nivel=4;
-        String palabra="BALLENA HOLA COMO";
+        String palabra="G E ";
         String eliminacion;
             /* Series_numericas test= new Series_numericas(longitud,nivel); */
-          /*   Scanner teclado = new Scanner(System.in);
+          Scanner teclado = new Scanner(System.in);
             palabra=teclado.nextLine();
-            cadena_caracteres test2= new cadena_caracteres(palabra,nivel); */
-            letras test3= new letras();
-
+            cadena_caracteres test2= new cadena_caracteres(palabra,nivel); 
+            arrays letras=new arrays(longitud,palabra,'-');
 
     }
 }
--- src/poliretos/arrays.java
+++ src/poliretos/arrays.java
@@ -2,11 +2,32 @@ package poliretos;
 
 public class arrays{
     public int tamanio;
-    public String caracter;
-    public arrays(){
+    public char caracter;
+    public char letra1;
+    public char letra2;
+    public String frase;
+    private char[][][] matriz_letras;
 
+    public arrays(int tamanio,String frase,char caracter){
+        this.frase=frase.toLowerCase();
+        this.tamanio=tamanio;
+        this.caracter=caracter;
+        letra1=this.frase.charAt(0);
+        letra2=this.frase.charAt(frase.indexOf(' ')+1);
+        letras Generador = new letras(tamanio,letra1,letra2,caracter);
+        matriz_letras=Generador.matriz();
+        imprimir();
     }
-    private void letras(){
-
+    private void imprimir(){
+            for (int filas = 0; filas < tamanio; filas++) {
+                for (int columnas = 0; columnas < tamanio; columnas++) {
+                    System.out.print(matriz_letras[0][filas][columnas]);
+                }
+                System.out.print("        ");
+                for (int columnas = 0; columnas < tamanio; columnas++) {
+                    System.out.print(matriz_letras[1][filas][columnas]);
+                }
+                System.out.println();
+            }
     }
 }
