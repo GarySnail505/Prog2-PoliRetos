95c133d57b559c63031cead3e9d82f9955b42a9e
garysnail <gary.defas@hotmail.com>
2024-11-04 22:07:30 -0500

    update de series caracteres

--- src/poli_retos/SeriesCaracteres.java
+++ src/poli_retos/SeriesCaracteres.java
@@ -17,43 +17,50 @@ public class SeriesCaracteres {
     
     // g2_serieC01
     public void g2_serieC01(){
-        System.out.println("\nSerie de caracteres (+ -):");
-        for (int i = 0; i < cantidadTerminos; i++) {
-            if(i%2==0){
-                System.out.print(" + ");
-            }else{
-                System.out.print(" - ");
+        System.out.print("\n- Serie C01:\n  ");
+        while (veces < tamanio) {
+            veces = veces + 2;
+            System.out.print("+ ");
+            if (veces <= tamanio) {
+                System.out.print("- ");
             }
         }
-        System.out.println("  ");
+        System.out.println("");
     }
     
     // g2_seriec02
     public void g2_serieC02() {
-        int fib1=1;
-        int fib2=1;
-        System.out.println("\nSerie de Fibonacci con signos (+):");
-        
-        if(cantidadTerminos>=1){
-            System.out.print("+ ");
+        j = 1;
+        veces = -1;
+        k = 0;
+        System.out.print("\n\n- Serie C02:\n ");
+
+        while (veces < tamanio) {
+            veces = veces + 2;
+            while (k < i) {
+                System.out.print("+");
+                k++;
+            }
+            k = 0;
+            System.out.print(" ");
+            j = i + j;
+            i = i + j;
+            if (veces <= tamanio) {
+                while (k < j) {
+                    System.out.print("+");
+                    k++;
+                }
+                k = 0;
+                System.out.print(" ");
+            }
         }
-        if(cantidadTerminos>=2){
-            System.out.print("+ ");
-        }
-        for(int i=3; i<=cantidadTerminos; i++){
-            int siguiente=fib1+fib2;
-            System.out.print("+".repeat(siguiente)+" ");
-            fib1=fib2;
-            fib2=siguiente;
-        }
-        System.out.println("  ");
     }
 
     
     // g2_serieC03
     public void g2_serieC03() {
         k = 0;
-        System.out.print("\n\n- serie C03. ++ +++ +++++ +++++++... (Primos):\n  ");
+        System.out.print("\n\n- Serie C03.\n  ");
 
         for (int contador = 0, numero = 2; contador < tamanio; numero++) {
             if (esPrimo(numero)) {
@@ -81,7 +88,7 @@ public class SeriesCaracteres {
     }
 
     public void g2_serieC04() {
-        System.out.print("\n\n- serie C04. + - * / + - * /... (Op. básicas):\n  ");
+        System.out.print("\n\n- Serie C04:\n  ");
         veces = 1;
         while (veces <= tamanio) {
             System.out.print("+ ");
@@ -103,7 +110,7 @@ public class SeriesCaracteres {
 
     public void g2_serieC05() {
         char[] caracteres = { '|', '\\', '|', '/' };
-        System.out.print("\n\n- serie C05. | \\ | / - | \\ | / -... (Vaivén):\n ");
+        System.out.print("\n\n- Serie C05.\n ");
         for (int i = 0; i < tamanio; i++) {
             for (int j = 0; j < caracteres.length; j++) {
                 System.out.print(caracteres[j] + " ");
@@ -112,12 +119,11 @@ public class SeriesCaracteres {
                 System.out.print("- ");
             }
         }
-        System.out.println();
     }
 
     public void g2_serieC06() {
         char letra = 'a';
-        System.out.print("\n\n- serie C06. a b c d e... (Abecedario):\n ");
+        System.out.print("\n\n- Serie C06.\n ");
         for (int i = 0; i < tamanio; i++) {
             System.out.print(letra + " ");
             letra++;
@@ -125,12 +131,11 @@ public class SeriesCaracteres {
                 letra = 'a';
             }
         }
-        System.out.println();
     }
 
     public void g2_serieCaracteres07() {
         int numeroRepeticiones = 2;
-        System.out.print("\n\n- serie C07.  aa bbbb cccccc dddddddd ... (Abecedario):\n ");
+        System.out.print("\n\n- Serie C07. \n ");
         for (int i = 0; i < tamanio; i++) {
             for (int j = 0; j < numeroRepeticiones; j++) {
                 System.out.print(letrasSeparadas[i]);
@@ -142,7 +147,7 @@ public class SeriesCaracteres {
 
     public void g2_serieCaracteres08() {
         int numeroRepeticiones = 1;
-        System.out.print("\n\n- serie C08. a bbb ccccc ddddddd eeeeeeeee... (Abecedario):\n ");
+        System.out.print("\n\n- Serie C08.\n ");
         for (int i = 0; i < tamanio; i++) {
             for (int j = 0; j < numeroRepeticiones; j++) {
                 System.out.print(letrasSeparadas[i]);
@@ -150,6 +155,7 @@ public class SeriesCaracteres {
             System.out.print(" ");
             numeroRepeticiones += 2;
         }
+        System.out.println("");
     }
 }
     // serie C09
