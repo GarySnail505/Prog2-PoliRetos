f9b40219f0f9b7e61961cd9388e7e8cd8eeed33d
Cristhian Carrillo <cristhiancarrillolocopaul@gmail.com>
2024-11-06 11:47:59 -0500

    menu_automatas

--- src/poli_retos/Automatas/Automata05.java
+++ src/poli_retos/Automatas/Automata05.java
@@ -5,23 +5,23 @@ public class Automata05 {
          public int error= -1;         
          public int aceptacion=25;
          public String frase;
-         public char[] alfabeto={'$','_',' ',',',';'};
+         public char[] alfabeto={'$','_',' ',',',';','=','-'};
          public char[] numero={'0','1','2','3','4','5','6','7','8','9'};
          public int [][]matriz_de_transicion=
-         {// l n $ _ "" , ; = enter
+         {// l n $ _ "" , ; = -
             {1,error,1,1,1,error,error,error,error},//q0
             {2,2,2,2,7,0,5,3,error},//q1
             {2,2,2,2,6,0,5,3,error},//q2
             {4,4,error,error,4,error,error,error,error},//q3
             {4,4,error,error,8,0,5,8,error},//q4
-            {error,error,error,error,error,error,error,error,aceptacion},//q5
+            {error,error,error,error,error,error,aceptacion,error,aceptacion},//q5
             {error,error,error,error,6,0,5,3,error},//q6
             {error,error,error,error,7,0,5,3,error},//q7
             {error,error,error,error,error,0,5,error,error},//q8
         };
 
     public Automata05(String frase) {
-        this.frase=frase;
+        this.frase=frase+"-";
         g2_compara_alfabeto();
     }
         
@@ -55,8 +55,6 @@ public class Automata05 {
                     System.out.println("La variable esta incorrectamente declarada");
                     break;
                 }
-                System.out.println(frase.charAt(i));
-                System.out.println(q);
             }
             if (q == aceptacion) {
                 System.out.println("La variable esta correctamente declarada");
