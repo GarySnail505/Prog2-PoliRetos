667af4c327e2fd2e8a5c4b0f50d446ed64d67124
Meli-u <melissa.guerra@epn.edu.ec>
2024-11-10 11:56:21 -0500

    Update Loadings.java

--- src/poli_retos/Loadings.java
+++ src/poli_retos/Loadings.java
@@ -194,7 +194,6 @@ public class Loadings {
                 System.out.println(" ");
             }
         }
-            System.out.println("Carga completa");
             System.out.println("[====================] 100%");
     }
 
